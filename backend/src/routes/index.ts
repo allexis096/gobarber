0d7ca060c0997d39df45c40e4a308cc0cfe323db
allexis096 <allexissoares@gmail.com>
2020-08-27 19:11:08 -0300

    :sparkles: feat: create UserService

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import appointmentsRoutes from './appointments.routes';
+import usersRouter from './user.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
+routes.use('/users', usersRouter);
 
 export default routes;
