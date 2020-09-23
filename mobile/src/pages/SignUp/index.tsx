2c67191de07bdfa2a9265098feb775723c0b6843
allexis096 <allexissoares@gmail.com>
2020-09-23 10:19:32 -0300

    :sparkles: feat: add routes between pages

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useNavigation } from '@react-navigation/native';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, BacktoSignIn, BacktoSignInText } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('ok');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BacktoSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BacktoSignInText>Voltar para logon</BacktoSignInText>
+      </BacktoSignIn>
+    </>
+  );
 };
 
 export default SignUp;
+
+// minute 2:47
