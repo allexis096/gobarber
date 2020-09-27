d8683a85c1ca06be7f899fe850501d5747f69313
allexis096 <allexissoares@gmail.com>
2020-09-27 18:44:38 -0300

    :hammer: refactor: appointments to ddd

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,18 +1,33 @@
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentsDTO';
 import IApointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import Appointment from '../entities/Appointment';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository
-  extends Repository<Appointment>
-  implements IApointmentsRepository {
+class AppointmentsRepository implements IApointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment;
+  }
+
+  public async create({
+    date,
+    provider_id,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
