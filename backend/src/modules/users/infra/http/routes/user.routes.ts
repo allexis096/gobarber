26235223ff3e1fcac58bf9d433c51a40da6ce8c2
allexis096 <allexissoares@gmail.com>
2020-09-27 19:13:05 -0300

    :hammer: refactor: users to ddd

--- backend/src/modules/users/infra/http/routes/user.routes.ts
+++ backend/src/modules/users/infra/http/routes/user.routes.ts
@@ -6,14 +6,17 @@ import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UsersRepository from '../../typeorm/repositories/UsersRepository';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
+const usersRepository = new UsersRepository();
+
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const createUser = new CreateUserService();
+  const createUser = new CreateUserService(usersRepository);
 
   const user = await createUser.execute({
     name,
@@ -31,7 +34,7 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
