c341cc0fec2ebd39aeb1a9a2712f187b4b4da9e4
allexis096 <allexissoares@gmail.com>
2020-09-29 19:06:25 -0300

    :sparkles: feat: add mail provider and tests to this

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,9 +1,14 @@
 import { container } from 'tsyringe';
 
-import DiskStorageProvider from './implementations/DiskStorageProvider';
-import IStorageProvider from './models/IStorageProvider';
+import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import IStorageProvider from './StorageProvider/models/IStorageProvider';
+
+// import FakeMailProvider from './MailProvider/fakes/FakeMailProvider';
+// import IMailProvider from './MailProvider/models/IMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
+
+// container.registerSingleton<IMailProvider>('MailProvider', FakeMailProvider);
