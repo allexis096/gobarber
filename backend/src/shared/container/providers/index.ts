cefbd92c2f819c28e86ea15dae9fc3f8ad9a5243
allexis096 <allexissoares@gmail.com>
2020-09-30 15:33:32 -0300

    :sparkles: feat: add EthrealMailProvider implementation

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,14 +1,17 @@
 import { container } from 'tsyringe';
 
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
+import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-// import FakeMailProvider from './MailProvider/fakes/FakeMailProvider';
-// import IMailProvider from './MailProvider/models/IMailProvider';
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EthrealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
-// container.registerSingleton<IMailProvider>('MailProvider', FakeMailProvider);
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
+);
