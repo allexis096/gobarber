729fc180c86379be42e65cbd95c0624621d5a510
allexis096 <allexissoares@gmail.com>
2020-10-05 11:15:04 -0300

    :heavy_plus_sign: chore: implement CacheProvider on ListProvidersService

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,9 +1,8 @@
 import { inject, injectable } from 'tsyringe';
 
-import AppError from '@shared/errors/AppError';
-
 import User from '@modules/users/infra/typeorm/entities/User';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 interface IRequest {
@@ -15,18 +14,27 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const user = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
 
-    if (!user) {
-      throw new AppError('User does not exists.');
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
     }
 
-    return user;
+    console.log('a query do banco foi feita!');
+
+    await this.cacheProvider.save(`providers-list:${user_id}`, users);
+
+    return users;
   }
 }
 
