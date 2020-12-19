335133df4d0212b419ad2650e6ae5edebf3ebe68
allexis096 <allexissoares@gmail.com>
2020-12-18 23:33:29 -0300

    :heavy_check_mark: test: creating SignIn page test

---
+++ frontend/src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+describe('SignIn Page', () => {
+  it('Should be able to sign in', () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonText = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonText);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+  });
+});
